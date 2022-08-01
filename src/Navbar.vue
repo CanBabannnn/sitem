cb3202499989d86d424df534fa2d6cccd69c1160
mishu <yc744604@gmail.com>
2022-08-02 00:57:34 +0300

    Update Navbar.vue

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="navbar sticky-nav mt-2">
         <a href="https://mishudev.xyz" style="font-size:25px;">Mishudev.xyz</a>
+	<button class="navbar-btns transition-shadow hover:shadow-md mr-1">Contact</button>
 	<button id="switch-mode-btn" class="navbar-btns switch-mode-btn transition-shadow hover:shadow-md mr-1"><i class="fa-solid fa-moon"></i></button>
     </div>
 </template>
